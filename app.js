8b5ea8ee54a7c16032ae77338ef969f2d30f0d56
Uryel Farias <uryel.farias@gmail.com>
2025-01-10 02:56:39 -0300

    inserindo o loop while à lógica do código

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 alert ('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 2;
+let numeroSecreto = 3;
 
-let chute = prompt('Escolha um número entre 1 e 3.');
+let chute; 
 
-if(numeroSecreto == chute){
-        console.log(`Você acertou! o número secreto era ${numeroSecreto}`);
-} else {
-    if(numeroSecreto > chute){
-        alert(`O número secreto é maior que ${chute}` )
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 5.');
+    
+    if(numeroSecreto == chute){
+            console.log(`Você acertou! o número secreto era ${numeroSecreto}`);
     } else {
-        alert(`O número secreto é menor que ${chute}`)
+        if(numeroSecreto > chute){
+            alert(`O número secreto é maior que ${chute}` )
+        } else {
+            alert(`O número secreto é menor que ${chute}`)
+        }
+        console.log("Você errou!");
     }
-    console.log("Você errou!");
 }
