d661a376ca30540b15b382eaa0f8c344e358f4ab
Uryel Farias <uryel.farias@gmail.com>
2025-01-10 00:06:14 -0300

    inserindo contatenação no código

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let numeroSecreto = 2;
 let chute = prompt('Escolha um número entre 1 e 3.');
 
 if(numeroSecreto == chute){
-    console.log("Você acertou!");
+        console.log(`Você acertou! o número secreto era ${numeroSecreto}`);
 } else {
     console.log("Você errou!");
 }
