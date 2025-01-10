22357cd6232f4cf91b1887aaf521c221ff436855
Uryel Farias <uryel.farias@gmail.com>
2025-01-09 23:51:21 -0300

    criando uma condição para verificar a entrada do usuário

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 alert ('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 2;
 
-prompt('Escolha um número entre 1 e 3.');
+let chute = prompt('Escolha um número entre 1 e 3.');
+
+if(numeroSecreto == chute){
+    console.log("Você acertou!");
+} else {
+    console.log("Você errou!");
+}
